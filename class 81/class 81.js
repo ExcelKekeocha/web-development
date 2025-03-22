dcc7b766305452810353a73a35de9e27c45564d9
ExcelKekeocha <exckek3993@humbleisd.net>
2025-03-22 18:05:45 -0500

    Add files via upload

---
+++ class 81/class 81.js
@@ -0,0 +1,12 @@
+//------------------------------------Libraries--------------------------------------------------------
+//Libraries means a file where lot of code is already there which we can use it in the program to make things quicker
+//p5.play.js(gaming functions), Bootstrap library(more CSS eg : slider, scrollbar, moving navigation bar),
+//moment.js(datetiem functions), speech.js(speak functions).
+
+//Whenveer we use any library we always have to connect.
+//Two ways to connect with any libarry
+//1. download the libary and upload in project we are going to work in
+//2. copy the cloud link from google. Search on google cloud link of library name. copy the link in html file.a
+
+//We have different other libraries like moment.js(time library) and speech.js(speech library).
+
---
+++ session 78/homework 78.js/homework 78.js
@@ -0,0 +1,16 @@
+var Excel= [20, 61, 0, 14, 3, 5, 9, 6, 12, 43]
+Excel.sort()
+document.write(Excel+'<br>')
+Excel.reverse()
+document.write( Excel+ "<br>")
+Excel.pop()
+document.write(Excel + "<br>")
+Excel.push("Hi")
+document.write(Excel + "<br>");
+arr2.shift()
+document.write(Excel + "<br>")
+Excel.unshift("Good")
+document.write(Excel + "<br>")
+document.write(Excel + slice(3) + "<br>")
+
+
